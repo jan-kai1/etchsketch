d7c2a55b4a1eeada3f3a84285e9b224b93e26fa0
test1 <android.agent.k@gmail.com>
2023-05-02 16:32:26 +0800

    finished

--- etchsketch.js
+++ etchsketch.js
@@ -1,24 +1,52 @@
 document.addEventListener("DOMContentLoaded", function ()
 {   
-    let resetButton = document.querySelector("#gridNumber")
+    let gridButton = document.querySelector("#gridNumber");
+    let resetButton = document.querySelector("#reset");
+    let grid = document.querySelector(".grid");
+    let existingRows = document.querySelectorAll(".gridrow");
+    // need to re initialize existing ROWS after creation
     gridnumber = 0;
     resetButton.addEventListener("click", function()
-    {   let grid = document.querySelector(".grid");
-        let existingRows = document.querySelectorAll(".gridrow");
+    {   
+        
+        
         if (existingRows.length > 0)
         {
-            for (existingRow of existingRows)
+            
+            deleteGrid(existingRows);
+        }
+       
+        else{
+            alert("no grid currently")
+        }
+    
+    });
+    
+    gridButton.addEventListener("click", function()
+    {
+
+        gridNumber = prompt("Grid Parameters?")
+            
+        createGrid(gridNumber);
+
+
+    })
+    //add event listeners for each 
+
+
+    function deleteGrid(existingRow)
+    {
+        for (existingRow of existingRows)
             {   
                 console.log(existingRow);
                 existingRow.parentElement.removeChild(existingRow);
             }
 
-        }
-       
-        else
-        {
-            gridNumber = prompt("Grid Parameters?")
-            if (gridNumber.length >0)
+    }
+    function createGrid(number)
+    {
+
+        if (gridNumber.length >0)
                {
                    
                    
@@ -34,6 +62,9 @@ document.addEventListener("DOMContentLoaded", function ()
                
                            gridSquare = document.createElement("div");
                            gridSquare.setAttribute('class', 'gridSquare');
+                           //create the event listener for each square
+                           drawListener(gridSquare);
+
                            gridRow.appendChild(gridSquare);
                
                        }
@@ -42,23 +73,32 @@ document.addEventListener("DOMContentLoaded", function ()
                    
    
                }
+        existingRows = document.querySelectorAll(".gridrow")
+    }
+
+    function drawListener(square)
+    {   
+        //make black is a function that makes the TARGET of the event
+        //event.target is the element which triggered the event
+        const makeBlack = (e) => e.target.style.backgroundColor = "black";
+        //2 event listeners for each square, one for initiating the draw (mousedown)
+        //one for continuing draw (mouseover)
+        //mousedown makes square black when clicked on
+        //event.buttons tracks the MOUSE button that is being held down, 
+        //if e.buttons ==1, LMB is held down, then makes the square hover over black
+        square.addEventListener("mousedown", makeBlack);
+        square.addEventListener("mouseover", e =>
+        {
+            if (e.buttons == 1) {
+                makeBlack(e)
 
 
+            }
+
+        });
 
 
-        }
-        
-
-    });
-    
-  
-    //add event listeners for each 
-
-
-
-
-
-
+    }
 
 
 
